dc2042e3f68fb735e16201ae034cdcd1f791b2e9
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-29 16:29:43 -0400

    created placeholder file for service for smite api interaction

---
+++ services/smite_api.rb
@@ -0,0 +1,12 @@
+class SmiteApi
+
+	OAUTH_BASE_URI = "http://api.smitegame.com/smiteapi.svc"
+
+	## API Calls
+
+	def get_something #sample api call
+		Rails.logger.info "pulling something...tbd")
+		#response = HTTParty.get()
+	end
+
+end
